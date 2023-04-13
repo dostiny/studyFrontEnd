766087a07b85967ae3e84a4115155898a44570b0
dostiny <dogyeom2@gmail.com>
2023-04-14 00:18:49 +0900

    라우터 끝

--- fresh/app/cart/layout.tsx
+++ fresh/app/cart/layout.tsx
@@ -1,13 +1,10 @@
 import "../globals.css";
-import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <body>
-        <p>현대카드 무이자이벤트중</p>
-        {children}
-      </body>
-    </div>
+    <>
+      <p>현대카드 무이자이벤트중</p>
+      {children}
+    </>
   );
 }
