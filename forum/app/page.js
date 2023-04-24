548c3c4508aa820edb43429045c54f67e734d9b7
dostiny <dogyeom2@gmail.com>
2023-04-25 00:39:33 +0900

    nextJS study

--- forum/app/page.js
+++ forum/app/page.js
@@ -1,3 +1,10 @@
-export default function Home() {
+import { connectDB } from "@/util/database";
+import { MongoClient } from "mongodb";
+
+export default async function Home() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
   return <div>안녕</div>;
 }
