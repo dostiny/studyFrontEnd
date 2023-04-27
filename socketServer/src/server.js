a2699de188811eaf36928a51020ce1dbe4cc295f
이도겸 <kweony1123@naver.com>
2023-04-27 15:35:21 +0900

    주석추가

--- socketServer/src/server.js
+++ socketServer/src/server.js
@@ -18,13 +18,13 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 /** 웹소켓 연결 (연결, 닫음, 메시지 받아오기, 메시지 보내기) */
-// wss.on("connection", (socket) => {
-//   console.log("Connected to Browser ⭕");
-//   socket.on("close", () => console.log("Disconnected to Browser ❌"));
-//   socket.on("message", (message) => {
-//     console.log(message.toString("utf8"));
-//   });
-//   socket.send("hello!!");
-// });
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ⭕");
+  socket.on("close", () => console.log("Disconnected to Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
