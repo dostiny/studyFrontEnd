47a941afdec2d5194df226df93ace1ed2de19e86
이도겸 <kweony1123@naver.com>
2023-04-27 15:34:58 +0900

    주석추가

--- socketServer/src/server.js
+++ socketServer/src/server.js
@@ -17,13 +17,14 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (socket) => {
-  console.log("Connected to Browser ⭕");
-  socket.on("close", () => console.log("Disconnected to Browser ❌"));
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
-  });
-  socket.send("hello!!");
-});
+/** 웹소켓 연결 (연결, 닫음, 메시지 받아오기, 메시지 보내기) */
+// wss.on("connection", (socket) => {
+//   console.log("Connected to Browser ⭕");
+//   socket.on("close", () => console.log("Disconnected to Browser ❌"));
+//   socket.on("message", (message) => {
+//     console.log(message.toString("utf8"));
+//   });
+//   socket.send("hello!!");
+// });
 
 server.listen(3000, handleListen);
